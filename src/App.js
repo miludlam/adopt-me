f110026e8c7b57915de759e96415a7ed34bf2a52
miludlam <miludlam@gmail.com>
2024-10-26 10:02:41 -0400

    Building a Pet component and relocating JS to App.js

---
+++ src/App.js
@@ -0,0 +1,40 @@
+const Pet = (props) => {
+    return React.createElement(
+        "div",
+        {},
+        [
+            React.createElement("h1", {}, props.name),
+            React.createElement("h2", {}, props.animal),
+            React.createElement("h2", {}, props.breed),
+        ]
+    );
+};
+
+const App = () => {
+    return React.createElement(
+        "div",
+        {},
+        [
+            React.createElement("h1", {}, "Adopt Me!"),
+            React.createElement(Pet, {
+                name: "Georgia",
+                animal: "Dog",
+                breed: "English Pointer"
+            }),
+            React.createElement(Pet, {
+                name: "Rip",
+                animal: "Dog",
+                breed: "English Pointer"
+            }),
+            React.createElement(Pet, {
+                name: "Brutus",
+                animal: "Dog",
+                breed: "Miniature Dachshund"
+            })
+        ]
+    );
+};
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
